f84078007c95769534b3f3b65a65600909ad75ef
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# ASPxFormLayout - How to save values to DB by using an Eval binding expression
+
+
+<p>It's possible to update data in DB by using the ASPxFormLayout with an Eval binding expression:</p><p>- Set the <a href="http://documentation.devexpress.com/#AspNet/DevExpressWebASPxFormLayoutLayoutItemBase_Nametopic">LayoutItemBase.Name</a>;</p><p>- Bind editors in LayoutItems with an Eval binding expression;</p><p>- Loop thru the layout items and set the SqlDataSource parameter default valuea depending on the  layout items names.</p>
+
+<br/>
+
+
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,27 @@
+﻿Imports Microsoft.VisualBasic
+Imports DevExpress.Web.ASPxEditors
+Imports DevExpress.Web.ASPxFormLayout
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Protected Sub btn_Click(ByVal sender As Object, ByVal e As EventArgs)
+		TryCast(fv.FindControl("fl"), ASPxFormLayout).ForEach(AddressOf ProcessItem)
+        'sds.Update() 'Uncomment this line to allow updating.
+	End Sub
+
+	Private Sub ProcessItem(ByVal item As LayoutItemBase)
+		Dim layoutItem As LayoutItem = TryCast(item, LayoutItem)
+		If layoutItem IsNot Nothing Then
+			Dim editBase As ASPxTextBox = TryCast(layoutItem.GetNestedControl(), ASPxTextBox)
+			If editBase IsNot Nothing Then
+				sds.UpdateParameters(layoutItem.FieldName).DefaultValue = editBase.Text
+			End If
+		End If
+	End Sub
+End Class
